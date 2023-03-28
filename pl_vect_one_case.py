e9b43ab60872276c934a1bb78c4fbbef31f7dce4
Niclas <niclas.persson01@gmail.com>
2023-03-28 11:02:57 +0200

    Added one case for testing (80% train, 20% test)

---
+++ pl_vect_one_case.py
@@ -0,0 +1,184 @@
+# ----------------------------------------------Import Packages------------------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR  # for building SVR model
+import sklearn.metrics as sm
+from gradients import compute_face_phi, dphidx, dphidy, init
+import time
+import sys
+import warnings
+import matplotlib.cbook
+# -------------------------------------------------start-------------------------------------------------
+warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
+
+# read data file
+st = time.process_time()
+tec = np.genfromtxt("large_wave/tec_large.dat", dtype=None, comments="%")
+
+print("Starting script")
+# text='VARIABLES = X Y P U V u2 v2 w2 uv eps'
+# Define variables from text-file
+x = tec[:, 0]  # Cell center x
+y = tec[:, 1]  # Cell center y
+p = tec[:, 2]  # Pressure
+u = tec[:, 3]  # Instant/laminar velocity in x
+v = tec[:, 4]  # Instant/laminar velocity in y
+uu = tec[:, 5]  # Stress
+vv = tec[:, 6]  # Stress
+ww = tec[:, 7]  # Stress
+uv = tec[:, 8]  # Stress
+k_DNS = 0.5 * (uu + vv + ww)  # Turbulent kinetic energy (?)
+eps_DNS = tec[:, 9]  # Dissipation
+
+# Define matrix dimensions
+if max(y) == 1.:
+    ni = 170
+    nj = 194
+    nu = 1. / 10000.
+else:
+    nu = 1. / 10595.
+    if max(x) > 8.:
+        nj = 162
+        ni = 162
+    else:
+        ni = 402
+        nj = 162
+
+viscos = nu
+
+# Reshape vectors to wanted dimensions
+u2d = np.reshape(u, (nj, ni))
+v2d = np.reshape(v, (nj, ni))
+p2d = np.reshape(p, (nj, ni))
+x2d = np.reshape(x, (nj, ni))
+y2d = np.reshape(y, (nj, ni))
+uu2d = np.reshape(uu, (nj, ni))  # =mean{v'_1v'_1}
+uv2d = np.reshape(uv, (nj, ni))  # =mean{v'_1v'_2}
+vv2d = np.reshape(vv, (nj, ni))  # =mean{v'_2v'_2}
+ww2d = np.reshape(ww, (nj, ni))
+k2d = np.reshape(k_DNS, (nj, ni))  # =mean{0.5(v'_iv'_i)}
+eps_DNS2d = np.reshape(eps_DNS, (nj, ni))
+
+u2d = np.transpose(u2d)
+v2d = np.transpose(v2d)
+p2d = np.transpose(p2d)
+x2d = np.transpose(x2d)
+y2d = np.transpose(y2d)
+uu2d = np.transpose(uu2d)
+vv2d = np.transpose(vv2d)
+ww2d = np.transpose(ww2d)
+uv2d = np.transpose(uv2d)
+k2d = np.transpose(k2d)
+eps_DNS2d = np.transpose(eps_DNS2d)
+
+# set Neumann of p at upper and lower boundaries
+p2d[:, 1] = p2d[:, 2]
+p2d[:, -1] = p2d[:, -1 - 1]
+
+# set periodic b.c on west boundary
+# SHOULD WE USE THIS?
+u2d[0, :] = u2d[-1, :]
+v2d[0, :] = v2d[-1, :]
+p2d[0, :] = p2d[-1, :]
+uu2d[0, :] = uu2d[-1, :]
+
+# x and y are fo the cell centers. The dphidx_dy routine needs the face coordinate, xf2d, yf2d
+# load them
+xc_yc = np.loadtxt("large_wave/mesh_large.dat")
+xf = xc_yc[:, 0]
+yf = xc_yc[:, 1]
+xf2d = np.reshape(xf, (nj - 1, ni - 1))
+yf2d = np.reshape(yf, (nj - 1, ni - 1))
+xf2d = np.transpose(xf2d)
+yf2d = np.transpose(yf2d)
+
+# compute cell centers
+xp2d = 0.25 * (x2d[0:-1, 0:-1] + x2d[0:-1, 1:] + x2d[1:, 0:-1] + x2d[1:, 1:])
+yp2d = 0.25 * (y2d[0:-1, 0:-1] + y2d[0:-1, 1:] + y2d[1:, 0:-1] + y2d[1:, 1:])
+
+# delete last row
+x2d = np.delete(x2d, -1, 0)
+y2d = np.delete(y2d, -1, 0)
+xp2d = np.delete(xp2d, -1, 0)
+yp2d = np.delete(yp2d, -1, 0)
+
+# delete last columns
+x2d = np.delete(x2d, -1, 1)
+y2d = np.delete(y2d, -1, 1)
+xp2d = np.delete(xp2d, -1, 1)
+yp2d = np.delete(yp2d, -1, 1)
+
+# compute geometric quantities
+areaw, areawx, areawy, areas, areasx, areasy, vol, fx, fy = init(x2d, y2d, xp2d, yp2d)
+
+# delete last row
+u2d = np.delete(u2d, -1, 0)
+v2d = np.delete(v2d, -1, 0)
+p2d = np.delete(p2d, -1, 0)
+k2d = np.delete(k2d, -1, 0)
+uu2d = np.delete(uu2d, -1, 0)
+vv2d = np.delete(vv2d, -1, 0)
+ww2d = np.delete(ww2d, -1, 0)
+uv2d = np.delete(uv2d, -1, 0)
+eps_DNS2d = np.delete(eps_DNS2d, -1, 0)
+
+# delete first row
+u2d = np.delete(u2d, 0, 0)
+v2d = np.delete(v2d, 0, 0)
+p2d = np.delete(p2d, 0, 0)
+k2d = np.delete(k2d, 0, 0)
+uu2d = np.delete(uu2d, 0, 0)
+vv2d = np.delete(vv2d, 0, 0)
+ww2d = np.delete(ww2d, 0, 0)
+uv2d = np.delete(uv2d, 0, 0)
+eps_DNS2d = np.delete(eps_DNS2d, 0, 0)
+
+# delete last columns
+u2d = np.delete(u2d, -1, 1)
+v2d = np.delete(v2d, -1, 1)
+p2d = np.delete(p2d, -1, 1)
+k2d = np.delete(k2d, -1, 1)
+uu2d = np.delete(uu2d, -1, 1)
+vv2d = np.delete(vv2d, -1, 1)
+ww2d = np.delete(ww2d, -1, 1)
+uv2d = np.delete(uv2d, -1, 1)
+eps_DNS2d = np.delete(eps_DNS2d, -1, 1)
+
+# delete first columns
+u2d = np.delete(u2d, 0, 1)
+v2d = np.delete(v2d, 0, 1)
+p2d = np.delete(p2d, 0, 1)
+k2d = np.delete(k2d, 0, 1)
+uu2d = np.delete(uu2d, 0, 1)
+vv2d = np.delete(vv2d, 0, 1)
+ww2d = np.delete(ww2d, 0, 1)
+uv2d = np.delete(uv2d, 0, 1)
+eps_DNS2d = np.delete(eps_DNS2d, 0, 1)
+
+ni = ni - 2
+nj = nj - 2
+
+# eps at last cell upper cell wrong. fix it.
+eps_DNS2d[:, -1] = eps_DNS2d[:, -2]
+
+# compute face value of U and V
+u2d_face_w, u2d_face_s = compute_face_phi(u2d, fx, fy, ni, nj)  # Error with dimensions for u2d vs fx,fy
+v2d_face_w, v2d_face_s = compute_face_phi(v2d, fx, fy, ni, nj)
+
+# x derivatives
+dudx = dphidx(u2d_face_w, u2d_face_s, areawx, areasx, vol)
+dvdx = dphidx(v2d_face_w, v2d_face_s, areawx, areasx, vol)
+
+# y derivatives
+dudy = dphidy(u2d_face_w, u2d_face_s, areawy, areasy, vol)
+dvdy = dphidy(v2d_face_w, v2d_face_s, areawy, areasy, vol)
+
+print("Data read")
+print("Starting ML")
+# ----------------------------------------------Project-Group Work---------------------------------------------------
+# ----------------------------------------------ML-Method Original Case----------------------------------------------
+omega = eps_DNS2d / k2d / 0.09
+
+# Compute C_my and ||duidxj|| to train model
+cmy_DNS = np.array(-uv2d / (k2d * (dudy + dvdx)) * omega)
