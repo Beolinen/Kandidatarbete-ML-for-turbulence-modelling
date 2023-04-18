c165e6a1ec3ec9a25a8baea19864fe55e340f9fd
Benjamin <benjamin.elmjonsson@gmail.com>
2023-04-18 11:39:59 +0200

    Pytorch

--- ML_Channel_pytorch.py
+++ ML_Channel_pytorch.py
@@ -7,6 +7,7 @@ import warnings
 import matplotlib.cbook
 
 #----------------Parameters----------------
+
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 
 viscos=1/5200
@@ -44,7 +45,6 @@ diss1=0.09*k*om
 ustar=(viscos*u[0]/y[0])**0.5
 yplus=y*ustar/viscos
 
-
 #-----------------Data_manipulation--------------------
 
 # Delete first value for all interesting data
@@ -67,6 +67,7 @@ tau = viscous_t/abs(uv_DNS)
 # Array for storing c_1, c_2, & c_3
 c = np.zeros((3, len(k_DNS)))
 
+#Estimate parameters for each data-point
 for i in range(len(k_DNS)):
 
     # Equation 14.2
@@ -81,32 +82,48 @@ for i in range(len(k_DNS)):
     B_pinv = np.linalg.pinv(B_matrix)
     c[:,i] = np.dot(B_pinv, a_vector).flatten()
 
-
-# Calculate c_1 average
+# Calculate c_1,c_2,c_3 average - Not expected values
 c_1_avg = np.mean(c[0,:])
 c_2_avg = np.mean(c[1,:])
 c_3_avg = np.mean(c[2,:])
-print("c_1 average: ", c_1_avg)
-print("c_2 average: ", c_2_avg)
-print("c_3 average: ", c_3_avg)
+
+#TODO ML using PyTorch to estimate c_1, c_2, & c_3
 
 
-# ML using pytorch to estimate c_1, c_2, & c_3
+#Needed?
+def forward(x):
+    """Function that makes a prediction for a given x, using weights and bias as defined below
+    """
+    w = torch.Tensor([[3.0],[4.0]], requires_grad = True)
+    b = torch.Tensor([[1.0]], requires_grad = True)
 
+    y_pred = torch.mm(x,w) + b
+    
+    return y_pred
 
+#Calculating target using "NEVM" (Non-linear Eddy Viscosity Model)
+# dont really know what option ↓↓ to use, all give more or less bad results (with or without ( + 2/3)*k_DNS)
 
+# uu = ((1/12)*(tau*dudy_DNS)**2*(c[0,:] + 6*c[1,:] + c[2,:]) + 2/3)*k_DNS
+# vv = ((1/12)*(tau*dudy_DNS)**2*(c[0,:] - 6*c[1,:] + c[2,:]) + 2/3)*k_DNS
+# ww = ((1/12)*(tau*dudy_DNS)**2*(-2*c[0,:] - 2*c[2,:]) + 2/3)*k_DNS
 
-#Calculating target using approximation
-uu = (1/12)*(tau[i]*dudy_DNS[i])**2*(c[0,:] + 6*c[1,:] + c[2,:])
-vv = (1/12)*(tau[i]*dudy_DNS[i])**2*(c[0,:] - 6*c[1,:] + c[2,:])
-ww = (1/12)*(tau[i]*dudy_DNS[i])**2*(-2*c[0,:] - 2*c[2,:])
+uu = ((1/12)*(tau*dudy_DNS)**2*(c_1_avg + 6*c_2_avg + c_3_avg) + 2/3)*k_DNS
+vv = ((1/12)*(tau*dudy_DNS)**2*(c_1_avg - 6*c_2_avg + c_3_avg) + 2/3)*k_DNS
+ww = ((1/12)*(tau*dudy_DNS)**2*(2*c_1_avg - 2*c_3_avg) + 2/3)*k_DNS
 
+#Standard values for c_1,c_2,c_3, results in catastrophic plots (?)
+
+# uu = ((1/12)*(tau*dudy_DNS)**2*(-0.05 + 6*0.11 + 0.21) + 2/3)*k_DNS
+# vv = ((1/12)*(tau*dudy_DNS)**2*(-0.05 - 6*0.11 + 0.21) + 2/3)*k_DNS
+# ww = ((1/12)*(tau*dudy_DNS)**2*(-2*-0.05 - 2*0.21) + 2/3)*k_DNS
 
 #-----------------Plotting--------------------
 
+#All kind of bad
 fig1 = plt.figure()
 plt.subplots_adjust(left=0.25,bottom=0.20)
-plt.plot(uu,yplus_DNS,'b-',label='RANS')
+plt.plot(uu,yplus_DNS,'b-',label='NEVM')
 plt.plot(uu_DNS,yplus_DNS,'r--',label='DNS')
 plt.axis([0, 10, 0, 5200])
 plt.xlabel("$\overline{u'u'}^+$")
@@ -115,22 +132,20 @@ plt.legend(loc="best",fontsize=16)
 
 fig2 = plt.figure()
 plt.subplots_adjust(left=0.25,bottom=0.20)
-plt.plot(abs(vv),yplus_DNS,'b-',label='RANS')
+plt.plot(abs(vv),yplus_DNS,'b-',label='NEVM')
 plt.plot(vv_DNS,yplus_DNS,'r--',label='DNS')
-plt.axis([0, 3, 0, 5200])
+plt.axis([0, 6, 0, 5200])
 plt.xlabel("$\overline{v'v'}^+$")
 plt.ylabel("$y^+$")
 plt.legend(loc="best",fontsize=16)
 
 fig3 = plt.figure()
 plt.subplots_adjust(left=0.25,bottom=0.20)
-plt.plot(abs(ww),yplus_DNS,'b-',label='RANS')
+plt.plot(abs(ww),yplus_DNS,'b-',label='NEVM')
 plt.plot(ww_DNS,yplus_DNS,'r--',label='DNS')
-plt.axis([0, 3, 0, 5200])
+plt.axis([0, 5, 0, 5200])
 plt.xlabel("$\overline{w'w'}^+$")
 plt.ylabel("$y^+$")
 plt.legend(loc="best",fontsize=16)
 
 plt.show()
-
-#u'u' decent, w'w' decent, v'v' bad, not expected
